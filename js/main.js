7493484181ada120be5ccecaa897e85d2ae7eadc
lazizbek29devoloper <esonboyevlazizbek29@gmail.com>
2024-03-26 09:19:28 +0500

    Add url copy button click handler.Wrap form submit handler in if stamement

--- js/main.js
+++ js/main.js
@@ -2,9 +2,32 @@ const elFormWrapperInner = document.querySelector(".js-form-wrapper__inner");
 const elFormWrapperInnerResults = document.querySelector(
   ".form-wrapper__inner-results"
 );
+const elCopyShortButtonlink = document.querySelector(
+  ".js-copy-short-button-link"
+);
+const elFormWrapper = document.querySelector(".form-wrapper");
 
-elFormWrapperInner.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+if (elFormWrapperInner) {
+  elFormWrapperInner.addEventListener("submit", function (evt) {
+    evt.preventDefault();
 
-  elFormWrapperInnerResults.classList.add("form-wrapper__inner-results-open");
-});
+    elFormWrapperInnerResults.classList.add("form-wrapper__inner-results-open");
+  });
+}
+
+if (elFormWrapper) {
+  elFormWrapper.addEventListener("click", function (evt) {
+    if (evt.target.matches(".js-copy-short-button-link")) {
+      // Cange button text
+      evt.target.textContent = "Copied!";
+      //
+      evt.target.classList.add("form-wrapper__copy-button--copied");
+
+      // Rest button text and class after 1 secund
+      setTimeout(function () {
+        evt.target.textContent = "Copy";
+        evt.target.classList.remove("form-wrapper__copy-button--copied");
+      }, 1000);
+    }
+  });
+}
